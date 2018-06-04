a63fa0e308282aa25463018daa32c904ea5b6ad1
garzuzo <garzuxo@gmail.com>
2018-06-04 15:03:03 -0500

    acc regex

--- CompetitiveProgramming/src/uva/IdentifyingLegalPascalRealConstants.java
+++ CompetitiveProgramming/src/uva/IdentifyingLegalPascalRealConstants.java
@@ -1,10 +1,25 @@
 package uva;
 
+
+import java.io.*;
+import java.util.*;
+
 public class IdentifyingLegalPascalRealConstants {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		while(in.hasNext()) {
+			
+			String act=in.nextLine();
+			
+			if(act.equals("*"))break;
+			act=act.trim();
+			boolean answ=act.matches("[+-]?\\d+(\\.\\d+([eE][+-]?\\d+)?|[eE][+-]?\\d+)");
+			System.out.println(act+(answ?" is legal.":" is illegal."));
+		}
+		
 	}
 
 }
